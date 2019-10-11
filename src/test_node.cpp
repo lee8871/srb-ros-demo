aea1e2f37947cbd57eeaf8af5d2788b669f7444f
lee_Ubuntu <lee8871@126.com>
2019-10-11 23:07:21 +0800

    将USB发送改在主函数，方便进行距离计算

--- src/test_node.cpp
+++ src/test_node.cpp
@@ -1,10 +1,8 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
 #include "geometry_msgs/Twist.h"
 
 #include "./cLogger/cLogger.h"
 #include "./json/Json.h"
-
 
 #include "lee.h"
 #include "UsbToSrb.h"
@@ -14,35 +12,38 @@
 #include "./common_cluster/BaseCluster.h"
 #include "./Nodes/MotorX2/NodeMotorX2.h"
 
+
+
+#include <cmath>
+#include <memory>
+
+
 using namespace srb::usb_bus;
 using namespace srb;
 
-
-NodeMotorX2 * key_ctrl_DUMOTOR=nullptr ;
-UsbToSrb* mainbusUB=nullptr;
-SrbMaster* mainSRBM=nullptr;
+using float64=double;
 
 
 
-void chatterCallback(const geometry_msgs::Twist& msg)
+int agvCmdToMotor(float64 linear,float64 angular){
+  float64 speed_temp;
+  speed_temp = 100*(linear+angular);
+  if(speed_temp>10){speed_temp+=50;}
+  if(speed_temp<-10){speed_temp-=50;}
+  return round(speed_temp);
+}
+float64 Cmd_liner = 0;
+float64 Cmd_angular = 0;
+int Cmd_Upgrade = 0;
+
+void cmdrelCallback(const geometry_msgs::Twist& msg)
 {
   ROS_DEBUG("I heard: (%f,%f)", msg.linear.x, msg.angular.z);
-  key_ctrl_DUMOTOR->Data()->ma.brake = no;
-  key_ctrl_DUMOTOR->Data()->mb.brake = no;
+  Cmd_liner = msg.linear.x;
+  Cmd_angular = msg.angular.z;
+  Cmd_Upgrade = 0;
 
-  int speed_temp;
-  speed_temp = 100*(msg.linear.x+msg.angular.z);
-  if(speed_temp>10){speed_temp+=50;}
-  if(speed_temp<-10){speed_temp-=50;}
-  key_ctrl_DUMOTOR->Data()->ma.speed = speed_temp;
 
-  speed_temp = 100*(msg.linear.x-msg.angular.z);
-  if(speed_temp>10){speed_temp+=50;}
-  if(speed_temp<-10){speed_temp-=50;}
-  key_ctrl_DUMOTOR->Data()->mb.speed = speed_temp;
-
-  key_ctrl_DUMOTOR->sendAccess(0);
-  mainbusUB->doAccess();
 }
 int main(int argc, char **argv)
 {
@@ -58,7 +59,6 @@ int main(int argc, char **argv)
    */
   ros::init(argc, argv, "SRB_test");
   ros::NodeHandle n;
-
   //FOLLOWING IS SRB CODING
   //To setting the SRB, you shold give name of the SRB bus and agv motor drive.
 	std::string usb_port_name;
@@ -70,8 +70,10 @@ int main(int argc, char **argv)
     ROS_ERROR("node name should set in bus_name.");
   }
   //Initialization a USB bus, and create a SrbMaster
-  mainbusUB = new UsbToSrb();
-  mainSRBM = new SrbMaster(mainbusUB);
+	auto mainbusUB = std::make_unique<UsbToSrb>();
+	auto mainSRBM = std::make_unique<SrbMaster>(mainbusUB.get());
+  //mainbusUB = new UsbToSrb();
+  //mainSRBM = new SrbMaster(mainbusUB);
 
   auto rev = mainbusUB->openUsbByName(usb_port_name.c_str());
 	if (rev != done) {
@@ -80,26 +82,47 @@ int main(int argc, char **argv)
 	}
   ROS_INFO( "Open port: [%s] done!\n", usb_port_name.c_str());
 	mainSRBM->scanNodes();
-
   Node * node  = mainSRBM->getNode(dumotor_name.c_str());
   if(node ==nullptr){
 		ROS_ERROR( "Get node [%s] fail!\n", dumotor_name.c_str());return 0;
 	}
-	key_ctrl_DUMOTOR = static_cast<NodeMotorX2*>(node);
+	auto key_ctrl_DUMOTOR = static_cast<NodeMotorX2*>(node);
+
+
+
   /**
    * ros::spin() will enter a loop, pumping callbacks.  With this version, all
    * callbacks will be called from within this thread (the main one).  ros::spin()
    * will exit when Ctrl-C is pressed, or the node is shutdown by the master.
    */
-  ros::Subscriber sub = n.subscribe("cmd_vel", 1000, chatterCallback);
+  ros::Subscriber sub = n.subscribe("cmd_vel", 1000, cmdrelCallback);
 
 
 
-  ros::Rate loop_rate(10);
+  ros::Rate loop_rate(100);
   int count = 0;
   while (ros::ok())  {
+    if(Cmd_Upgrade==0){
+      key_ctrl_DUMOTOR->Data()->ma.brake = no;
+      key_ctrl_DUMOTOR->Data()->mb.brake = no;
+      key_ctrl_DUMOTOR->Data()->ma.speed = agvCmdToMotor(Cmd_liner,Cmd_angular);
+      key_ctrl_DUMOTOR->Data()->mb.speed = agvCmdToMotor(Cmd_liner,-Cmd_angular);
+      Cmd_Upgrade++;
+    }
+    else if(Cmd_Upgrade != 20){//for 200ms
+      Cmd_Upgrade++;
+    }
+    else{
+      key_ctrl_DUMOTOR->Data()->ma.brake = no;
+      key_ctrl_DUMOTOR->Data()->mb.brake = no;
+      key_ctrl_DUMOTOR->Data()->ma.speed = 0;
+      key_ctrl_DUMOTOR->Data()->mb.speed = 0;
+    }
 
-    chatter_pub.publish(msg);
+    key_ctrl_DUMOTOR->sendAccess(0);
+    mainbusUB->doAccess();
+
+
 
     ros::spinOnce();
     loop_rate.sleep();
